e8f3a5fd9b763e05d874167113dfb34e4f908eb5
김재균 <worbs0604@gmail.com>
2023-08-24 17:06:31 +0900

    Update 줌회의영상다운로드매크로.py

--- 프로젝트/줌회의영상다운로드매크로.py
+++ 프로젝트/줌회의영상다운로드매크로.py
@@ -7,6 +7,7 @@ fm = 1
 sS = 2
 mS = 3
 lS = 5
+lsS = 5
 llS = 10
 강의이름 = None
 강의날짜 = None
@@ -50,14 +51,12 @@ async def 정보가져오기():
     topic_element = await page.query_selector('span.meeting-topic') # 회의 주제(타이틀) 가져오기
     if topic_element:
         강의이름 = await topic_element.inner_text()
-    date_selector = 'span.mgr-md' # 날짜 값 가져오기
-    date_element = await page.query_selector(date_selector)
+    date_element = await page.query_selector('span.mgr-md') # 날짜 값 가져오기
     if date_element:
         date_text = await date_element.inner_text() # 정규식을 사용하여 날짜 값을 추출
         pattern = r'(\d+년 \d+월 \d+일)'
         matches = re.search(pattern, date_text)
-        if matches:
-            강의날짜 = matches.group(1)
+        강의날짜 = matches.group(1)
         폴더이름 = f"{강의이름}_{강의날짜}" # 폴더 경로 설정 및 생성
         폴더정보 = os.path.join(폴더경로, 폴더이름)
         os.makedirs(폴더정보, exist_ok=True)
@@ -73,9 +72,9 @@ async def 영상다운(이름):
             if handle != page:
                 new_handle = handle
                 break
-        await asyncio.sleep(sS)
-    dl_element = await new_handle.query_selector('a.download-btn')
-    await asyncio.sleep(mS)
+        await asyncio.sleep(mS)
+    dl_element = await new_handle.query_selector('a.download-btn') # 다운로드 버튼을 찾기
+    await asyncio.sleep(sS)
     if dl_element:
         async with new_handle.expect_download() as download_info:
             await dl_element.click()
@@ -89,29 +88,27 @@ async def 영상다운(이름):
     
 async def 삭제():
     global page  # 전역 변수로 사용
-    delete_element = await page.query_selector('button:nth-child(3)')
+    delete_element = await page.query_selector('button:nth-child(3)') # 삭제
     if delete_element:
-        await delete_element.click()
+        await delete_element.click() # 버튼 누르기
         await asyncio.sleep(mS)
-        delete1_selector = 'button.zm-button--primary.zm-button--small.zm-button > span'
-        await asyncio.sleep(sS)
-        delete1_element = await page.query_selector(delete1_selector)
+        delete1_element = await page.query_selector('button.zm-button--primary.zm-button--small.zm-button > span') # 휴지통으로 이동
         if delete1_element: 
-            await page.get_by_role("button", name="휴지통으로 이동").click()
+            await page.get_by_role("button", name="휴지통으로 이동").click() # 버튼 누르기
             await asyncio.sleep(mS)
             await page.wait_for_load_state()            
     await asyncio.sleep(sS)
     await page.reload() 
-    await asyncio.sleep(llS)
+    await asyncio.sleep(lsS)
     
     
 async def 동작():
     global browser, page, item  # 전역 변수로 사용
-    async with async_playwright() as playwright:
+    async with async_playwright() as playwright: # 크로미움(크롬)으로 브라우저 열기
         browser = await playwright.chromium.launch(
             headless=False,
         )
-        page = await browser.new_page(accept_downloads=True)
+        page = await browser.new_page(accept_downloads=True) # 새로운 창이 열리면 page에 저장
         await 로그인()
         while fm:
             await 정보가져오기()
