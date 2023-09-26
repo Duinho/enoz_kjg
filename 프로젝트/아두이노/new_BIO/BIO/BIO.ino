e0fee61e82b7afd36067ed0c6b3e1c9c86abe505
김재균 <worbs0604@gmail.com>
2023-09-26 16:58:29 +0900

    2

--- 프로젝트/아두이노/new_BIO/BIO/BIO.ino
+++ 프로젝트/아두이노/new_BIO/BIO/BIO.ino
@@ -12,7 +12,7 @@ int choice = 0;
 
 
 int BRIGHTNESS = 30;  // LED 밝기
-Adafruit_NeoPixel strip(12, 8, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip(16, 8, NEO_GRB + NEO_KHZ800);
 
 char NEO_1 = "", NEO_2 = "", NEO_3 = "", NEO_4 = "";
 
@@ -40,11 +40,28 @@ void setup() {
 }
 
 void loop() {
-  if (!digitalRead(IN)) {
-    BUTTON_C();
-    NEO_C();
-  } else {
-    RESET();
+  /*
+    if (!digitalRead(IN)) {
+      BUTTON_C();
+      NEO_C();
+    } else {
+      RESET();
+    }*/
+  for (int j = 0; j < 8; j++) {
+    for (int i = 0; i < 4; i++) {
+      NEO_V(i, NEO_ARR[0][j]);
+    }
+    for (int i = 4; i < 8; i++) {
+      NEO_V(i, NEO_ARR[1][j]);
+    }
+    for (int i = 8; i < 12; i++) {
+      NEO_V(i, NEO_ARR[2][j]);
+    }
+    for (int i = 12; i < 16; i++) {
+      NEO_V(i, NEO_ARR[3][j]);
+    }
+    strip.show();
+    delay(1000);
   }
   //NEO_V(9, 'R');
 }
@@ -86,18 +103,18 @@ void NEO_C() {
   NEO_1 = NEO_ARR[0][NEO_1C], NEO_2 = NEO_ARR[1][NEO_2C], NEO_3 = NEO_ARR[2][NEO_3C], NEO_4 = NEO_ARR[3][NEO_4C];
   NEO_V(3, NEO_1);
   if (NEO_1 == 'B' && NEO_2 == 'R' && NEO_3 == 'G' && NEO_4 == 'O') {
-    digitalWhite(OUT, HIGH);
+    digitalWrite(OUT, HIGH);
     BUT_C = 0;
   } else {
-    digitalWhite(OUT, LOW);
+    digitalWrite(OUT, LOW);
   }
 }
 
 void NEO_V(int i, char j) {
-  if (j == 'R') strip.setPixelColor(i, BRIGHTNESS, 0, 0);
-  if (j == 'G') strip.setPixelColor(i, 0, BRIGHTNESS, 0);
-  if (j == 'B') strip.setPixelColor(i, 0, 0, BRIGHTNESS);
-  if (j == 'O') strip.setPixelColor(i, BRIGHTNESS, BRIGHTNESS / 1.82, 0);
+  if (j == 'R') strip.setPixelColor(i, 255, 0, 0);
+  if (j == 'G') strip.setPixelColor(i, 0, 0, 255);
+  if (j == 'B') strip.setPixelColor(i, 0, 255, 0);
+  if (j == 'O') strip.setPixelColor(i, 255, 0, 39);
   if (j == 'X') strip.setPixelColor(i, 0, 0, 0);
 }
 
