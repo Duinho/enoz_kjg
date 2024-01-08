c578df0132f5c1dc495291bac5c193c4a50413cf
김재균 <worbs0604@gmail.com>
2024-01-08 14:53:42 +0900

    1

---
+++ 프로젝트/AI분류기/test.py
@@ -0,0 +1,62 @@
+import cv2
+from ultralytics import YOLO
+
+CONFIDENCE_THRESHOLD = 0.2 # 인식하는 유사치 비율
+GREEN = (0, 255, 0)        
+WHITE = (255, 255, 255)
+
+class_list = ['clam', 'crab', 'octopus', 'oyster']   # 바지락, 꽃게, 낙지, 굴
+
+model = YOLO('C:/Users/enoz00/runs/detect/train5/weights/best.pt')   # 인공지능 모델 경로
+
+cap = cv2.VideoCapture(0)                   # 카메라 기능 켜기
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)      # 카메라 너비 640
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)     # 카메라 높이 480
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break  # 카메라 에러 시 바로 종료
+
+    detection = model(frame)[0]
+
+    clam_detected = False    # 대상 인식 여부를 추적하기 위한 변수
+    crab_detected = False
+    octopus_detected = False
+    oyster_detected = False
+
+    for data in detection.boxes.data.tolist():
+        confidence = float(data[4])
+        if confidence < CONFIDENCE_THRESHOLD:    # 유사치 비율이 0.2보다 낮다면 아래 코드를 넘김
+            continue
+
+        xmin, ymin, xmax, ymax = int(data[0]), int(data[1]), int(data[2]), int(data[3])     # 인식되는 부분에 네모 박스를 침
+        label = int(data[5])
+
+        if class_list[label] == 'clam':
+            clam_detected = True  
+        if class_list[label] == 'crab':
+            crab_detected = True  
+        if class_list[label] == 'octopus':
+            octopus_detected = True  
+        if class_list[label] == 'oyster':
+            oyster_detected = True  
+
+        cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), GREEN, 2)
+        cv2.putText(frame, class_list[label] + ' ' + str(round(confidence * 100)) + '%', (xmin, ymin), cv2.FONT_ITALIC, 1, WHITE, 2)
+
+    if clam_detected:
+        print("clam detected!")  # 대상이 인식되면 메시지 출력
+    if crab_detected:
+        print("crab detected!")     
+    if octopus_detected:
+        print("octopus detected!")  
+    if oyster_detected:
+        print("oyster detected!")  
+    cv2.imshow('frame', frame)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
