06bbbf62cf47dde56bd417499043e2487b935dcf
김재균 <worbs0604@gmail.com>
2024-06-25 18:01:20 +0900

    12

--- 프로젝트/파이썬/코딩교실/만족도조사/만족도조사.py
+++ 프로젝트/파이썬/코딩교실/만족도조사/만족도조사.py
@@ -5,7 +5,7 @@ from docx.shared import RGBColor
 import win32com.client as win32
 from playwright.sync_api import sync_playwright
 import numpy as np
-
+import time
 result = 0
 page = None
 browser = None
@@ -25,19 +25,47 @@ def 자동화(page):
     global col_9
     for i in range(len(col_9)):  # col_9에 저장된 개수만큼 반복
         page.goto(만족도조사링크)  # 만족도조사 링크로 이동
+        time.sleep(1)
+        page.click('span.NPEfkd.RveJvd.snByac')
+        time.sleep(1)
         page.wait_for_selector('span.NPEfkd.RveJvd.snByac', timeout=10000)  # 제출 버튼이 나올 때까지 대기
         col_a = np.random.choice([5, 4], size=8, p=[0.95, 0.05])  # 95%로 5점과 5%로 4점을 col_a에 8개 저장
-
         for j in range(1, len(col_a)):
             col_a[j] += 5 * j  # col_a에 0 5 10 15로 순차적으로 더함(5개 항목)
-
+        time.sleep(1)
         elements = page.query_selector_all('.AB7Lab.Id5V1')  # 현재 페이지에 있는 모든 클릭 항목들을 찾음
-
+        
         for k in range(0, 8):  # 8번 반복함 (총 8문제)
             elements[col_a[k] - 1].click()  # 클릭 항목을 순차적으로 체크함
 
         page.fill('textarea[aria-labelledby="i33"]', str(col_9[i]))  # 마지막 주관식에 엑셀에 col_9에 있는 값을 적음
-        page.click('span.NPEfkd.RveJvd.snByac')  # 제출 버튼을 누름
+        elements = page.query_selector_all('span.NPEfkd.RveJvd.snByac')
+        for element in elements:
+            if element.inner_text() == '다음':
+                element.click()
+                break
+        time.sleep(1)
+        col_a = np.random.choice([5, 4], size=8, p=[0.95, 0.05])  # 두 번째 섹션을 위해 col_a를 다시 초기화
+        for j in range(1, len(col_a)):
+            col_a[j] += 5 * j  # col_a에 0 5 10 15로 순차적으로 더함(5개 항목)
+            
+        elements = page.query_selector_all('.AB7Lab.Id5V1')  # 현재 페이지에 있는 모든 클릭 항목들을 찾음
+        for k in range(0, 7): 
+            elements[col_a[k] - 1].click()  # 클릭 항목을 순차적으로 체크함
+        
+        elements = page.query_selector_all('span.NPEfkd.RveJvd.snByac')
+        for element in elements:
+            if element.inner_text() == '다음':
+                element.click()
+                break
+        time.sleep(1)
+        elements = page.query_selector_all('span.NPEfkd.RveJvd.snByac')
+        for element in elements:
+            if element.inner_text() == '제출':
+                element.click()
+                break
+        time.sleep(1)
+
 
 def 동작():
     global browser, page  # 전역 변수로 사용
