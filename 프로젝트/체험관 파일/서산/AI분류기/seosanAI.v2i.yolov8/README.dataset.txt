cf219364a3fd7050766831fb8a13fec1355bd50f
김재균 <worbs0604@gmail.com>
2025-04-24 14:14:43 +0900

    1
    1

--- 프로젝트/체험관 파일/서산/AI분류기/seosanAI.v2i.yolov8/README.dataset.txt
+++
@@ -1,6 +0,0 @@
-# seosanAI > 2024-01-05 2:06pm
-https://universe.roboflow.com/seosan-ai/seosanai
-
-Provided by a Roboflow user
-License: CC BY 4.0
-
--- 프로젝트/체험관 파일/예산/AI분류기/train.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-
-# 모델을 로드하세요.
-model = YOLO('yolov8n.yaml')  # YAML에서 새 모델 구축
-
-# 모델을 훈련합니다.
-results = model.train(data='C:/Users/enoz00/Desktop/seosanAI.v2i.yolov8/data.yaml', epochs=100, imgsz=640, batch_size=16)
